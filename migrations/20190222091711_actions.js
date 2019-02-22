3b01cbc38093d2f1b0bbe13089124821ce74019d
Lenna <lenna@lennamau.com>
2019-02-22 09:51:55 -0700

    migrations completed

--- migrations/20190222091711_actions.js
+++ migrations/20190222091711_actions.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", tbl => {
+    tbl.increments();
+    tbl.string("description").notNullable();
+    tbl.text("notes").notNullable();
+    tbl.boolean("completed").defaultTo(false);
+    tbl.integer('project_id').unsigned()
+    tbl
+      .foreign("project_id")
+      .references("id")
+      .on("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
